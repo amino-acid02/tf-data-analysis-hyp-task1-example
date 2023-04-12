d6d2c55e43397a09fcbf2ba3b94ee820317f47c5
amino-acid02 <125069090+amino-acid02@users.noreply.github.com>
2023-04-12 14:03:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,10 +4,7 @@ from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 1022285388 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
     successes = np.array([x_success, y_success])
     samples = np.array([x_cnt, y_cnt])
     test = proportions_ztest(count=successes, nobs=samples,  alternative='smaller')
